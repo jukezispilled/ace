98b7d7d8b1aad1f2676ca75833e929bfc0791992
Jack Anderson <whotfisjax@gmail.com>
2024-07-30 22:32:19 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ function App() {
 
   return (
     <div className="h-screen w-screen flex justify-center items-center overflow-clip bg-zinc-950">
+          <div className='absolute top-3 right-5 flex space-x-3 items-center z-[50] text-2xl font-custom text-slate-300'>
+              <a href="https://x.com/" className='transition ease-in-out duration-150'>
+                Twitter
+              </a>
+              <a href="https://t.me/" className='transition ease-in-out duration-150'>
+                Telegram
+              </a>
+          </div>
       <video
         ref={videoRef}
         autoPlay
